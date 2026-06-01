75ac49c7b3e5a827aece707b1ebe1b4b5136d46a
csh4bj <csh4bj@virginia.edu>
2026-06-01 19:23:06 +0000

    saving our two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+USER=csh4bj@virginia.edu
+NAME=csh4bj
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
